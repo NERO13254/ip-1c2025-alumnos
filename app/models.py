8fa02abe540f2f36afed2e2c04b92e837bd96509
santiago <@contacto.ssnt@gmail.com>
2025-06-21 23:47:43 -0300

    Funcion de favoritos, y detalles esteticos(buscador, y navbar)

--- app/models.py
+++ app/models.py
@@ -1,23 +1,18 @@
 from django.db import models
 from django.conf import settings
 
-
 class Favourite(models.Model):
-    # Detalles del pokemon.
-    id = models.IntegerField(primary_key=True, blank=True) #ID de pokeapi
-    name = models.CharField(max_length=200)  # Nombre del personaje
-    height = models.CharField(max_length=200)  # Altura
-    weight = models.CharField(max_length=200)  # Peso
-    base_experience = models.IntegerField(null=True, blank=True)  # Experiencia base
-    types = models.JSONField(default=list)  # Lista de tipos (ej: ["grass", "poison"])
-    image = models.URLField()  # URL de la imagen
-
-    # Asociamos el favorito con el usuario que lo guarda.
+    pokeapi_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    height = models.CharField(max_length=200)
+    weight = models.CharField(max_length=200)
+    base_experience = models.IntegerField(null=True, blank=True)
+    image = models.URLField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
-        # Restringe duplicados: un mismo usuario no puede guardar el mismo personaje varias veces.
-        unique_together = (('user', 'name'),)
+        unique_together = (('user', 'pokeapi_id'),)
+
 
     def __str__(self):
         return (f"{self.name} - Altura: {self.height if self.height else 'Desconocida'} "
